30525c03efe0eb285f3422f2b4ace9f6a1bbfd42
Paul Popat <paul@popatux.co.uk>
2024-05-15 14:23:08 +0100

    Added token group response

--- src/parser/token-group.ts
+++ src/parser/token-group.ts
@@ -1,5 +1,6 @@
 import { ParserError } from "./error";
 import { Token } from "./token";
+import { TokenGroupResponse } from "./token-group-response";
 
 export class TokenGroup {
   readonly #tokens: Array<Token>;
@@ -41,5 +42,47 @@ export class TokenGroup {
 
   Skip(count: number) {
     return new TokenGroup(this.#tokens, this.#index + count);
+  }
+
+  Build<TData extends Record<string, any>, TResponse>(
+    actions: {
+      [TKey in keyof TData]: (
+        context: TokenGroup
+      ) => TokenGroupResponse<TData[TKey]>;
+    },
+    builder: (
+      data: TData,
+      context: TokenGroup
+    ) => TResponse | TokenGroupResponse<TResponse>
+  ): TokenGroupResponse<TResponse> {
+    let context: TokenGroup = this;
+    const data: Partial<TData> = {};
+    for (const key in actions) {
+      const response = actions[key](context);
+      context = response.Context;
+      data[key] = response.Response;
+    }
+
+    const result = builder(data as TData, context);
+    if (result instanceof TokenGroupResponse) return result;
+    return new TokenGroupResponse(context, result);
+  }
+
+  Map<TItem, TResult>(
+    input: TItem[],
+    mapper: (
+      context: TokenGroup,
+      item: TItem,
+      index: number
+    ) => TokenGroupResponse<TResult>
+  ) {
+    return input.reduce((ctx, n, i) => {
+      const result = mapper(ctx.Context, n, i);
+
+      return new TokenGroupResponse(result.Context, [
+        ...ctx.Response,
+        result.Response,
+      ]);
+    }, new TokenGroupResponse(this, [] as Array<TResult>));
   }
 }
